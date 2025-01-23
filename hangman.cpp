997b27af31766360437ad624e3245d60fd03e88b
Debasish Das <149887980+RaptorDot@users.noreply.github.com>
2025-01-23 14:58:59 +0000

    something feels off

--- hangman.cpp
+++ hangman.cpp
@@ -1,11 +1,50 @@
 #include<iostream>
 #include<vector>
+#include<ctime>
+#include<cstdlib>
+#include<fstream>
+#include <bits/stdc++.h>
 using namespace std;
+
+vector<string> loadWords(){
+    vector<string> words;
+    string line;
+    ifstream fin("wordList.txt");
+    while (getline(fin, line))
+    {
+        if (line.length()>0)
+        {
+            words.push_back(line);
+        }   
+    }
+    return words;
+}
+
+
 string chooseWord(){
+    vector<string> word_list = loadWords();
+    srand(time(0));
+
+    int random_index = rand()%word_list.size();
+
+    return word_list[random_index];
 
 }
-bool displayWords(string word,const vector<char>gussedL){
+bool displayWords(string word,const vector<char>guessedL){
 
+    bool winStatus = true;
+    for (char c: word)
+    {
+        if (find(guessedL.begin() , guessedL.end(), c)!=guessedL.end())
+        {
+            cout<<c<<" ";
+            winStatus = false;
+        }
+        
+    }
+        cout<<endl;
+
+    return winStatus;
 }
 int main(){
     string wordToGuess = chooseWord();
